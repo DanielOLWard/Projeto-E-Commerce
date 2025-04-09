5c9f6ffc4b9833c24d1d33265e01da4f93b5fffe
fic <fic@NOTE04-S28>
2025-04-08 21:52:57 -0300

    API criada

---
+++ E-Commerce_API/E-Commerce_API/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E_Commerce_API.Models;
+
+public partial class Produto
+{
+    public int IdProduto { get; set; }
+
+    public string? NomeProduto { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public decimal? Preco { get; set; }
+
+    public int? QtdEstoque { get; set; }
+
+    public string? Categoria { get; set; }
+
+    public string? Imagem { get; set; }
+
+    public virtual ICollection<ItemPedido> ItemPedidos { get; set; } = new List<ItemPedido>();
+}
